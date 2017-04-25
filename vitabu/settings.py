dc95ce6eb3b4b8667209d2038fb071e1733f5515
bmwasaru <bmwasaru@gmail.com>
2017-04-25 13:03:33 +0300

    add heroku url to allowed hosts

--- vitabu/settings.py
+++ vitabu/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'vitabu.herokuapp.com']
 
 
 # Application definition
